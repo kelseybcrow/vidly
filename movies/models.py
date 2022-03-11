9b9e2e85b9c75557601164db2f0493ff4c61cc56
Kelsey Crow <kelseybcrow@gmail.com>
2022-03-11 09:13:54 -0700

    adding genres

--- movies/models.py
+++ movies/models.py
@@ -1,10 +1,13 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 
 
 class Genre(models.Model):
     name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
 
 
 class Movie(models.Model):
@@ -13,3 +16,4 @@ class Movie(models.Model):
     number_in_stock = models.IntegerField()
     daily_rate = models.FloatField()
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(default=timezone.now)
